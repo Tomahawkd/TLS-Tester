649594cc8d4e94274af88292a41ad9f9e3c72356
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-03-31 20:34:36 +0800

    obtain testfile from testssl and delete testfile

--- src/io/tomahawkd/Main.java
+++ src/io/tomahawkd/Main.java
@@ -1,12 +1,13 @@
 package io.tomahawkd;
 
 import io.tomahawkd.testssl.Analyzer;
+import io.tomahawkd.testssl.ExecutionHelper;
 
 public class Main {
 
 	public static void main(String[] args) {
 		try {
-			var t = Analyzer.parseFile("./testfile/test.json");
+			var t = Analyzer.parseFile(ExecutionHelper.runTest("www.baidu.com"));
 			t.print();
 			//t.forEach((ip, seg) -> Analyzer.analyzeLeakyChannel(seg));
 
