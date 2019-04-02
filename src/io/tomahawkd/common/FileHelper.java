105186f82a14a3c4cab3075f82b186d2316d2d44
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-02 21:16:04 +0800

    file helper now can write file

--- src/io/tomahawkd/common/FileHelper.java
+++ src/io/tomahawkd/common/FileHelper.java
@@ -1,10 +1,7 @@
 package io.tomahawkd.common;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.charset.Charset;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.FileSystemException;
 
@@ -27,6 +24,23 @@ public class FileHelper {
 	public static boolean isDirExist(String path) {
 		var file = new File(path);
 		return file.exists() && file.isDirectory();
+	}
+
+	public static void writeFile(String path, String data, boolean overwrite) throws IOException {
+		var file = new File(path);
+		if (file.exists() && overwrite)
+			if (!file.delete()) throw new FileSystemException(TAG + "File cannot be deleted.");
+
+		if (!file.exists()) {
+			if (!file.createNewFile()) throw new FileSystemException(TAG + "File cannot be created.");
+		} else throw new FileAlreadyExistsException(TAG + " File already exist.");
+
+		try (FileOutputStream out = new FileOutputStream(file)) {
+			out.write(data.getBytes(StandardCharsets.UTF_8));
+			out.flush();
+		} catch (IOException e) {
+			throw new IOException(TAG + e.getMessage());
+		}
 	}
 
 	public static void createDir(String path) throws IOException {
