78cdcd6bfc9e1ac92389b8d7b5d5b2ec95ef55c3
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-03 11:44:00 +0800

    extract to helper function

---
+++ src/io/tomahawkd/detect/AnalyzerHelper.java
@@ -0,0 +1,65 @@
+package io.tomahawkd.detect;
+
+import io.tomahawkd.common.ShodanQueriesHelper;
+import io.tomahawkd.testssl.Analyzer;
+import io.tomahawkd.testssl.ExecutionHelper;
+import io.tomahawkd.testssl.data.SegmentMap;
+import io.tomahawkd.testssl.data.TargetSegmentMap;
+import io.tomahawkd.testssl.data.parser.OfferedResult;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+class AnalyzerHelper {
+
+	public static final String TAG = "[AnalyzerHelper]";
+
+	static boolean isVulnerableTo(SegmentMap target, String tag) {
+		var t = target.get(tag).getResult();
+		if (t == null) throw new IllegalArgumentException("No vulnerability tag");
+		return ((OfferedResult) t).isResult();
+	}
+
+	static boolean isOtherWhoUseSameCertVulnerableTo(SegmentMap target,
+	                                                 Function<SegmentMap, Boolean> detect) {
+		return isOtherWhoUseSameCertVulnerableTo(target, detect, null);
+	}
+
+	static boolean isOtherWhoUseSameCertVulnerableTo(SegmentMap target,
+	                                                 Function<SegmentMap, Boolean> detect,
+	                                                 Map<String, Boolean> cache) {
+
+		var serialNumber = (String) target.get("cert_serialNumber").getResult();
+
+		try {
+			AtomicBoolean isVul = new AtomicBoolean(false);
+			var list = ShodanQueriesHelper.searchIpWithSerial(serialNumber);
+			list.forEach(e -> {
+
+				if (cache != null && cache.containsKey(e)) {
+					isVul.set(cache.get(e));
+					return;
+				}
+
+				try {
+					var file = ExecutionHelper.runTest(e);
+					TargetSegmentMap map = Analyzer.parseFile(file);
+
+					map.forEach((ip, segmentMap) -> {
+						if (detect.apply(target)) {
+							isVul.set(true);
+							if (cache != null) cache.put(ip, true);
+						}
+					});
+				} catch (Exception ex) {
+					throw new IllegalArgumentException(TAG + " " + ex.getMessage());
+				}
+			});
+
+			return isVul.get();
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e.getMessage());
+		}
+	}
+}
--- src/io/tomahawkd/detect/LeakyChannelAnalyzer.java
+++ src/io/tomahawkd/detect/LeakyChannelAnalyzer.java
@@ -1,16 +1,10 @@
 package io.tomahawkd.detect;
 
-import io.tomahawkd.common.ShodanQueriesHelper;
-import io.tomahawkd.testssl.Analyzer;
-import io.tomahawkd.testssl.ExecutionHelper;
 import io.tomahawkd.testssl.data.SectionType;
 import io.tomahawkd.testssl.data.SegmentMap;
-import io.tomahawkd.testssl.data.TargetSegmentMap;
 import io.tomahawkd.testssl.data.parser.CipherInfo;
-import io.tomahawkd.testssl.data.parser.OfferedResult;
 import io.tomahawkd.testssl.data.parser.PreservedCipherList;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -21,11 +15,26 @@ public class LeakyChannelAnalyzer {
 
 	private static Map<String, Boolean> cache = new HashMap<>();
 
-	public static boolean checkVulnerable(SegmentMap target, boolean shouldContinue) {
+	public static boolean checkVulnerable(SegmentMap target) {
 		System.out.println("Checking " + target.getIp());
 		var isVul = isHostRSAVulnerable(target);
 		cache.put(target.getIp(), isVul);
-		return isRSAUsed(target) && (isVul || isOtherRSAVulnerable(target, shouldContinue));
+		return isRSAUsed(target) && (isVul || isOtherRSAVulnerable(target));
+	}
+
+	static boolean isRSAUsedInAnyVersion(SegmentMap target) {
+		var list = target.getByType(SectionType.CIPHER_ORDER);
+		var count = 0;
+		for (var current : list) {
+
+			AtomicBoolean hasRSA = new AtomicBoolean(false);
+			((CipherInfo) current.getResult()).getCipher().getList().forEach(e -> {
+				if (e.getKeyExchange().contains("RSA")) hasRSA.set(true);
+			});
+			if (hasRSA.get()) count++;
+		}
+
+		return count > 0;
 	}
 
 	private static boolean isRSAUsed(SegmentMap target) {
@@ -37,62 +46,16 @@ public class LeakyChannelAnalyzer {
 		if (cipher == null) throw new IllegalArgumentException(TAG + " Cipher not found");
 		var preferred = cipher.getKeyExchange().contains("RSA");
 
-		// Part II
-		var list = target.getByType(SectionType.CIPHER_ORDER);
-		var count = 0;
-		for (var current : list) {
-			AtomicBoolean hasRSA = new AtomicBoolean(false);
-			((CipherInfo) current.getResult()).getCipher().getList().forEach(e -> {
-				if (e.getKeyExchange().contains("RSA")) hasRSA.set(true);
-			});
-			if (hasRSA.get()) count++;
-		}
-
-		return preferred || count > 0;
+		return preferred || isRSAUsedInAnyVersion(target);
 	}
 
-	private static boolean isHostRSAVulnerable(SegmentMap target) {
-		var robot = target.get("ROBOT").getResult();
-		if (robot == null) throw new IllegalArgumentException("No vulnerability tag");
-		var drown = target.get("DROWN").getResult();
-		if (drown == null) throw new IllegalArgumentException("No vulnerability tag");
-
-		return ((OfferedResult) robot).isResult() || ((OfferedResult) drown).isResult();
+	static boolean isHostRSAVulnerable(SegmentMap target) {
+		return AnalyzerHelper.isVulnerableTo(target, "ROBOT") ||
+				AnalyzerHelper.isVulnerableTo(target, "DROWN");
 	}
 
-	private static boolean isOtherRSAVulnerable(SegmentMap target, boolean shouldContinue) {
-
-		// should not continue for dead loop
-		if (!shouldContinue) return false;
-
-		var serialNumber = (String) target.get("cert_serialNumber").getResult();
-
-		// According to the source code, this method put the result into the map,
-		// so that we do not need to put it manually except this host self.
-		try {
-			AtomicBoolean isVul = new AtomicBoolean(false);
-			var list = ShodanQueriesHelper.searchIpWithSerial(serialNumber);
-			list.forEach(e -> isVul.set(cache.computeIfAbsent(e, k -> {
-
-				var singleVul = new AtomicBoolean(false);
-
-				try {
-					var file = ExecutionHelper.runTest(e);
-					TargetSegmentMap map = Analyzer.parseFile(file);
-
-					map.forEach((ip, segmentMap) -> {
-						if (isHostRSAVulnerable(segmentMap)) singleVul.set(true);
-					});
-				} catch (Exception ex) {
-					throw new IllegalArgumentException(TAG + " " + ex.getMessage());
-				}
-
-				return singleVul.get();
-			})));
-
-			return isVul.get();
-		} catch (Exception e) {
-			throw new IllegalArgumentException(e.getMessage());
-		}
+	private static boolean isOtherRSAVulnerable(SegmentMap target) {
+		return AnalyzerHelper.isOtherWhoUseSameCertVulnerableTo(target,
+				LeakyChannelAnalyzer::isHostRSAVulnerable, cache);
 	}
 }
