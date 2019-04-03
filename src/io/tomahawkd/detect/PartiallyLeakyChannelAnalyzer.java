f44aceb30a98b200ee97b4b9b8fbdf094fd528db
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-03 11:44:34 +0800

    init entry function

--- src/io/tomahawkd/detect/PartiallyLeakyChannelAnalyzer.java
+++ src/io/tomahawkd/detect/PartiallyLeakyChannelAnalyzer.java
@@ -1,4 +1,12 @@
 package io.tomahawkd.detect;
 
+import io.tomahawkd.testssl.data.SegmentMap;
+
 public class PartiallyLeakyChannelAnalyzer {
+
+	public static final String TAG = "[PartiallyLeakyChannelAnalyzer]";
+
+	public static boolean isPoodleTlsVulnerable(SegmentMap target) {
+		return false;
+	}
 }
