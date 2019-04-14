1942e443cec8e782bc2d94d408516c6aadaa5745
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-14 13:33:25 +0800

    update analyzer

--- src/io/tomahawkd/testssl/Analyzer.java
+++ src/io/tomahawkd/testssl/Analyzer.java
@@ -2,6 +2,8 @@ package io.tomahawkd.testssl;
 
 import io.tomahawkd.common.log.Logger;
 import io.tomahawkd.detect.LeakyChannelAnalyzer;
+import io.tomahawkd.detect.PartiallyLeakyChannelAnalyzer;
+import io.tomahawkd.detect.TaintedChannelAnalyzer;
 import io.tomahawkd.testssl.data.SegmentMap;
 
 public class Analyzer {
@@ -9,22 +11,16 @@ public class Analyzer {
 	private static final Logger logger = Logger.getLogger(Analyzer.class);
 
 	public static void analyze(SegmentMap target) {
-		if (LeakyChannelAnalyzer.checkVulnerability(target)) {
+		if (LeakyChannelAnalyzer.checkVulnerability(target))
 			logger.warn("Result: " + target.getIp() + " is vulnerable.");
-		} else {
-			logger.ok("Result: " + target.getIp() + " is not vulnerable.");
-		}
+		else logger.ok("Result: " + target.getIp() + " is not vulnerable.");
 
-//		if (TaintedChannelAnalyzer.checkVulnerable(target)) {
-//			logger.warn("Result: " + target.getIp() + " is vulnerable.");
-//		} else {
-//			logger.ok("Result: " + target.getIp() + " is not vulnerable.");
-//		}
-//
-//		if (PartiallyLeakyChannelAnalyzer.checkVulnerable(target)) {
-//			logger.warn("Result: " + target.getIp() + " is vulnerable.");
-//		} else {
-//			logger.ok("Result: " + target.getIp() + " is not vulnerable.");
-//		}
+		if (TaintedChannelAnalyzer.checkVulnerability(target))
+			logger.warn("Result: " + target.getIp() + " is vulnerable.");
+		else logger.ok("Result: " + target.getIp() + " is not vulnerable.");
+
+		if (PartiallyLeakyChannelAnalyzer.checkVulnerability(target))
+			logger.warn("Result: " + target.getIp() + " is vulnerable.");
+		else logger.ok("Result: " + target.getIp() + " is not vulnerable.");
 	}
 }
