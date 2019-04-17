f019e2251fe3b2f9959f6f1094d12b6dd595bf93
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-17 16:38:44 +0800

    update init

--- init.sh
+++ init.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-git submodule init
-git submodule update
+domain="$(cut -d' ' -f1 <<< `ls ./Subdomain-Detect | wc -l`)"
+tls="$(cut -d' ' -f1 <<< `ls ./TLS-Attacker | wc -l`)"
+test="$(cut -d' ' -f1 <<< `ls ./testssl.sh | wc -l`)"
+
+if [[ $domain -eq 0 -o $tls -eq 0 -o $test -eq 0 ]]; then
+    git submodule init
+    git submodule update
+fi
 
 cd ./TLS-Attacker
 mvn clean install
