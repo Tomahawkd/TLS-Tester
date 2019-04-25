c7e9aeee6003b6c76bda4320d1c3b67cf4ab452f
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-25 19:28:50 +0800

    use native file api

--- src/io/tomahawkd/common/log/LogFileOutputDelegate.java
+++ src/io/tomahawkd/common/log/LogFileOutputDelegate.java
@@ -1,9 +1,12 @@
 package io.tomahawkd.common.log;
 
-import io.tomahawkd.common.FileHelper;
-
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -22,15 +25,30 @@ public class LogFileOutputDelegate implements LogHandler.OutputDelegate {
 	}
 
 
-	private String file;
+	private Path filePath;
 
 	public LogFileOutputDelegate() {
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMMMddHHmmss");
-		this.file = path + dateFormat.format(new Date(System.currentTimeMillis())) + extension;
+		String file = path + dateFormat.format(new Date(System.currentTimeMillis())) + extension;
+		this.filePath = Paths.get(file);
+
+		File f = filePath.toFile();
+		try {
+			if (f.exists() && !f.delete()) throw new RuntimeException("Cannot delete existing file " + file);
+			if (!f.createNewFile()) throw new RuntimeException("Cannot create file " + file);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error when creating file " + file);
+		}
 	}
 
 	@Override
 	public void publish(String message) throws Exception {
-		FileHelper.writeFile(file, message + "\n", false);
+
+		message += "\n";
+
+		if (!filePath.toFile().exists())
+			Files.write(filePath, message.getBytes(StandardCharsets.UTF_8), StandardOpenOption.CREATE_NEW);
+		else Files.write(filePath, message.getBytes(StandardCharsets.UTF_8), StandardOpenOption.APPEND);
 	}
 }
