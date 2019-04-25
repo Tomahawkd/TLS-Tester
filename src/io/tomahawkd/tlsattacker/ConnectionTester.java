57675db82a539af9933bb25d8ad2b850e7f3d807
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-04-25 16:11:16 +0800

    use embedded util function

--- src/io/tomahawkd/tlsattacker/ConnectionTester.java
+++ src/io/tomahawkd/tlsattacker/ConnectionTester.java
@@ -4,28 +4,24 @@ import de.rub.nds.tlsattacker.core.config.Config;
 import de.rub.nds.tlsattacker.core.config.delegate.CiphersuiteDelegate;
 import de.rub.nds.tlsattacker.core.config.delegate.ClientDelegate;
 import de.rub.nds.tlsattacker.core.constants.CipherSuite;
-import de.rub.nds.tlsattacker.core.protocol.message.*;
+import de.rub.nds.tlsattacker.core.constants.HandshakeMessageType;
 import de.rub.nds.tlsattacker.core.state.State;
 import de.rub.nds.tlsattacker.core.workflow.DefaultWorkflowExecutor;
-import de.rub.nds.tlsattacker.core.workflow.WorkflowTrace;
 import de.rub.nds.tlsattacker.core.workflow.WorkflowTraceUtil;
-import de.rub.nds.tlsattacker.core.workflow.action.ReceiveAction;
-import de.rub.nds.tlsattacker.core.workflow.action.SendAction;
+import de.rub.nds.tlsattacker.core.workflow.factory.WorkflowTraceType;
 import io.tomahawkd.common.log.Logger;
+import io.tomahawkd.testssl.data.parser.CipherInfo;
 import org.jetbrains.annotations.Contract;
 
-import java.util.List;
-
-public class DowngradeTester {
+public class ConnectionTester {
 
 	private Config config;
-	private WorkflowTrace trace;
 
 	private static final String DEFAULT_PORT = "443";
 
-	private static final Logger logger = Logger.getLogger(DowngradeTester.class);
+	private static final Logger logger = Logger.getLogger(ConnectionTester.class);
 
-	public DowngradeTester(String host) {
+	public ConnectionTester(String host) {
 
 		if (host.split(":").length == 1) host = host + ":" + DEFAULT_PORT;
 
@@ -34,16 +30,17 @@ public class DowngradeTester {
 		ClientDelegate delegate = new ClientDelegate();
 		delegate.setHost(host);
 		delegate.applyDelegate(config);
-		trace = new WorkflowTrace();
-		trace.addTlsAction(new SendAction(new ClientHelloMessage(config)));
-		trace.addTlsAction(new ReceiveAction(
-				new ServerHelloMessage(),
-				new CertificateMessage(),
-				new ServerHelloDoneMessage()));
+		config.setWorkflowTraceType(WorkflowTraceType.HELLO);
 	}
 
 	@Contract("_ -> this")
-	public DowngradeTester setCipherSuite(CipherSuite cipherSuite) {
+	public ConnectionTester setNegotiateVersion(CipherInfo.SSLVersion version) {
+		config.setHighestProtocolVersion(version.getVersionForTest());
+		return this;
+	}
+
+	@Contract("_ -> this")
+	public ConnectionTester setCipherSuite(CipherSuite cipherSuite) {
 
 		logger.info("Set cipher suite " + cipherSuite.getValue());
 		CiphersuiteDelegate ciphersuiteDelegate = new CiphersuiteDelegate();
@@ -52,14 +49,15 @@ public class DowngradeTester {
 		return this;
 	}
 
-	public List<HandshakeMessage> execute() {
+	public boolean isServerHelloReceived() {
 
 		logger.debug("Executing...");
-		State state = new State(config, trace);
+		State state = new State(config);
 		DefaultWorkflowExecutor executor = new DefaultWorkflowExecutor(state);
 		executor.executeWorkflow();
 
 		logger.debug("Complete");
-		return WorkflowTraceUtil.getAllSendHandshakeMessages(trace);
+		return WorkflowTraceUtil
+				.didReceiveMessage(HandshakeMessageType.SERVER_HELLO, state.getWorkflowTrace());
 	}
 }
