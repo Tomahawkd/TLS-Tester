772241d90910386331c677695106d015a2ec8a0a
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-05-12 14:43:18 +0800

    switch block make it easier to work

--- src/io/tomahawkd/identifier/router/DLinkIdentifier.java
+++ src/io/tomahawkd/identifier/router/DLinkIdentifier.java
@@ -5,6 +5,7 @@ import com.fooock.shodan.model.host.Host;
 import io.tomahawkd.identifier.CommonIdentifier;
 
 public class DLinkIdentifier extends CommonIdentifier {
+
 	@Override
 	public String tag() {
 		return "D-Link";
@@ -13,8 +14,11 @@ public class DLinkIdentifier extends CommonIdentifier {
 	@Override
 	public boolean identify(Host host) {
 		for (Banner banner : host.getBanners()) {
-			if (banner.getPort() == 23) {
-				return banner.getData().contains("Dlink");
+
+			switch (banner.getPort()) {
+				case 21: return banner.getProduct().contains("D-Link");
+				case 23: return banner.getData().contains("Dlink");
+				default:
 			}
 		}
 
