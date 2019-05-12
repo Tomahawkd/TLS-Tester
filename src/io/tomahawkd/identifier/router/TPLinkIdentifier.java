7e3d8756a2fe7e739d46e8e822799a33154026e9
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2019-05-12 14:43:34 +0800

    add detection

--- src/io/tomahawkd/identifier/router/TPLinkIdentifier.java
+++ src/io/tomahawkd/identifier/router/TPLinkIdentifier.java
@@ -17,15 +17,16 @@ public class TPLinkIdentifier extends CommonIdentifier {
 	@Override
 	public boolean identify(Host host) {
 
-		for (Banner b : host.getBanners()) {
-			if (String.valueOf(b.getPort()).contains("443") ||
-					String.valueOf(b.getPort()).contains("80") ||
-					b.getPort() == 8888 ||
-					b.getPort() == 81 || b.getPort() == 82 || b.getPort() == 83 || b.getPort() == 84) {
+		for (Banner banner : host.getBanners()) {
+			if (isWebPort(banner.getPort())) {
 
-				Map<String, String> header = parseHttpHeader(b.getData());
+				Map<String, String> header = parseHttpHeader(banner.getData());
 				String result = header != null ? header.get("WWW-Authenticate") : "";
-				return result != null && result.contains("TP-Link");
+				return result != null && (
+						result.contains("TP-Link") ||
+						result.contains("TP-LINK") ||
+						result.contains("TL-") ||
+						banner.getTitle().contains("TL-"));
 			}
 		}
 
