9516ddc8080629e18a1f573bc5a5b87fcfc8ed9b
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-02-14 13:46:14 +0800

    provider parsed in the arg parser class, add ascii art and specifications

--- TLS-Tester/src/main/java/io/tomahawkd/Main.java
+++ TLS-Tester/src/main/java/io/tomahawkd/Main.java
@@ -1,11 +1,10 @@
 package io.tomahawkd;
 
 import de.rub.nds.tlsattacker.core.exceptions.TransportHandlerConnectException;
+import io.tomahawkd.analyzer.AnalyzerRunner;
 import io.tomahawkd.common.log.Logger;
-import io.tomahawkd.common.provider.FileTargetProvider;
 import io.tomahawkd.common.provider.TargetProvider;
 import io.tomahawkd.data.TargetInfo;
-import io.tomahawkd.analyzer.AnalyzerRunner;
 import io.tomahawkd.testssl.data.exception.FatalTagFoundException;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 
@@ -19,6 +18,7 @@ import java.util.concurrent.TimeUnit;
 public class Main {
 
 	private static final Logger logger = Logger.getLogger(Main.class);
+	private static final String version = "v2.0";
 
 	static {
 		Security.addProvider(new BouncyCastleProvider());
@@ -26,6 +26,7 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		System.out.println(title);
 		ArgParser.INSTANCE.parseArgs(args);
 
 		try {
@@ -34,7 +35,12 @@ public class Main {
 			ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(threadCount);
 
 //			ListTargetProvider<String> provider = new ListTargetProvider<>(ShodanExplorer.explore("has_ssl: true", 80));
-			TargetProvider<String> provider = FileTargetProvider.getDefault("./temp/test2.txt");
+//			TargetProvider<String> provider = FileTargetProvider.getDefault("./temp/test2.txt");
+			TargetProvider<String> provider = ArgParser.INSTANCE.get().getTargetDelegate();
+			if (provider == null) {
+				logger.fatal("Cannot parse a valid provider.");
+				return;
+			}
 
 			while (provider.hasMoreData()) {
 
@@ -76,4 +82,19 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
+
+
+	private static final String title =
+			" _________  ___       ________                _________  _______   ________  _________  _______   ________     \n" +
+					"|\\___   ___\\\\  \\     |\\   ____\\              |\\___   ___\\\\  ___ \\ |\\   ____\\|\\___   ___\\\\  ___ \\ |\\   __  \\    \n" +
+					"\\|___ \\  \\_\\ \\  \\    \\ \\  \\___|_  ___________\\|___ \\  \\_\\ \\   __/|\\ \\  \\___|\\|___ \\  \\_\\ \\   __/|\\ \\  \\|\\  \\   \n" +
+					"     \\ \\  \\ \\ \\  \\    \\ \\_____  \\|\\____________\\  \\ \\  \\ \\ \\  \\_|/_\\ \\_____  \\   \\ \\  \\ \\ \\  \\_|/_\\ \\   _  _\\  \n" +
+					"      \\ \\  \\ \\ \\  \\____\\|____|\\  \\|____________|   \\ \\  \\ \\ \\  \\_|\\ \\|____|\\  \\   \\ \\  \\ \\ \\  \\_|\\ \\ \\  \\\\  \\| \n" +
+					"       \\ \\__\\ \\ \\_______\\____\\_\\  \\                 \\ \\__\\ \\ \\_______\\____\\_\\  \\   \\ \\__\\ \\ \\_______\\ \\__\\\\ _\\ \n" +
+					"        \\|__|  \\|_______|\\_________\\                 \\|__|  \\|_______|\\_________\\   \\|__|  \\|_______|\\|__|\\|__|   " + version + "\n" +
+					"                        \\|_________|                                 \\|_________|                              \n" +
+					"                                                                                                               \n" +
+					"A TLS channel security tester by Tomahawkd@Github\n" +
+					"For more information please visit https://github.com/Tomahawkd/TLS-Tester\n" +
+					"Thanks to http://patorjk.com/software/taag for Console ASCII art\n";
 }
