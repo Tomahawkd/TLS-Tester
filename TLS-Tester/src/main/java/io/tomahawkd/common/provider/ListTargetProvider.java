705732ab3f948e32657eb8b074237ff4c9ef2595
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-02-29 15:28:32 +0800

    addAll is used in censys target provider, which need to update status

--- TLS-Tester/src/main/java/io/tomahawkd/common/provider/ListTargetProvider.java
+++ TLS-Tester/src/main/java/io/tomahawkd/common/provider/ListTargetProvider.java
@@ -1,5 +1,6 @@
 package io.tomahawkd.common.provider;
 
+import java.util.Collection;
 import java.util.List;
 
 public class ListTargetProvider<T> extends CommonTargetProvider<T> {
@@ -10,6 +11,16 @@ public class ListTargetProvider<T> extends CommonTargetProvider<T> {
 
 	public ListTargetProvider(List<T> list) {
 		addAll(list);
+		setStatus(State.FINISHING);
+	}
+
+	@Override
+	public synchronized void addAll(Collection<T> data) {
+		super.addAll(data);
+		setStatus(State.RUNNING);
+	}
+
+	public void setFinish() {
 		setStatus(State.FINISHING);
 	}
 }
