815d2c6afcf7a9cf3b6376cf92b80cd772eea60c
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-03-02 18:38:20 +0800

    since we could not set classpath to our new extension pack, we have to delegate the driver to bypass the DriverManager#isDriverAllowed check

--- TLS-Tester/src/main/java/io/tomahawkd/database/RecorderHandler.java
+++ TLS-Tester/src/main/java/io/tomahawkd/database/RecorderHandler.java
@@ -1,10 +1,14 @@
 package io.tomahawkd.database;
 
 import io.tomahawkd.ArgParser;
+import io.tomahawkd.common.ComponentsLoader;
 import io.tomahawkd.common.log.Logger;
-import org.reflections.Reflections;
 
 import java.lang.reflect.InvocationTargetException;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Set;
 
 public enum RecorderHandler {
 
@@ -13,7 +17,7 @@ public enum RecorderHandler {
 	private final Logger logger = Logger.getLogger(RecorderHandler.class);
 	private Recorder recorder;
 
-	RecorderHandler() {
+	public void init() {
 		open();
 	}
 
@@ -21,8 +25,23 @@ public enum RecorderHandler {
 		String name = ArgParser.INSTANCE.get().getDbType();
 		logger.debug("Searching target database entity.");
 
-		for (Class<?> clazz : new Reflections().getTypesAnnotatedWith(Database.class)) {
+		Set<Class<? extends Driver>> drivers = ComponentsLoader.INSTANCE.loadClasses(Driver.class);
+		for (Class<? extends Driver> driver : drivers) {
+			// ignore delegate itself
+			if (DriverDelegate.class.equals(driver)) continue;
+
+			logger.debug("Loading db driver " + driver.getName());
+			try {
+				DriverManager.registerDriver(new DriverDelegate(driver.newInstance()));
+			} catch (SQLException | InstantiationException | IllegalAccessException e) {
+				logger.warn("Unable to register driver " + driver.getName());
+				logger.warn(e.getMessage());
+			}
+		}
+
+		for (Class<?> clazz : ComponentsLoader.INSTANCE.loadClassesByAnnotation(Database.class)) {
 			if (Recorder.class.isAssignableFrom(clazz)) {
+				logger.debug("Checking Class " + clazz.getName());
 				if (clazz.getAnnotation(Database.class).name().equals(name)) {
 					try {
 						logger.debug("Invoking target database initialization procedure.");
@@ -35,6 +54,7 @@ public enum RecorderHandler {
 						} else {
 							recorder = (Recorder) clazz.newInstance();
 						}
+						break;
 					} catch (InstantiationException | IllegalAccessException |
 							NoSuchMethodException | InvocationTargetException e) {
 						logger.fatal("Invoking database initialization procedure failed.");
