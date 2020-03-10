5b570f9c7d7efefcba79c87b1fd43b5e42a4af9c
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-03-10 11:26:39 +0800

    rename to constants

---
+++ TLS-Tester/src/main/java/io/tomahawkd/database/RecorderConstants.java
@@ -0,0 +1,19 @@
+package io.tomahawkd.database;
+
+public class RecorderConstants {
+
+	// Constant data
+	public static final String TABLE_DATA = "data";
+	public static final String TABLE_STATISTIC = "statistic";
+
+	public static final String COLUMN_HOST = "host";
+	public static final String COLUMN_COUNTRY = "country";
+	public static final String COLUMN_IDENTIFIER = "identifier";
+	public static final String COLUMN_SSL = "ssl_enabled";
+	public static final String COLUMN_HASH = "hash";
+
+	public static final String COLUMN_TOTAL = "total";
+
+	public static final int TABLE = 0;
+	public static final int VIEW = 1;
+}
