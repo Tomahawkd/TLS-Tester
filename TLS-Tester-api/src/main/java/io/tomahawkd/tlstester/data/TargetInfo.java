c56ab62ba251b6f809d54c61ca933400377d98ec
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-03-20 13:15:34 +0800

    add target info description

--- TLS-Tester-api/src/main/java/io/tomahawkd/tlstester/data/TargetInfo.java
+++ TLS-Tester-api/src/main/java/io/tomahawkd/tlstester/data/TargetInfo.java
@@ -4,20 +4,65 @@ import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * <p>Host information storage class</p>
+ * <p>
+ * This class stores key information of the target host.
+ * {@link java.net.InetSocketAddress} stores target host and port.
+ * {@link TargetInfo#collectedData} stores collected data from {@link io.tomahawkd.tlstester.data.DataCollector}.
+ * {@link TargetInfo#analysisResult} stores test result from {@link io.tomahawkd.tlstester.analyzer.Analyzer}.
+ * </p>
+ *
+ * <p>
+ * After acquired target host from the provider, the class is instantiated with corresponding host.
+ * Next, the TLS-Tester proceeds to collect all of information using
+ * {@link io.tomahawkd.tlstester.data.DataCollector}
+ * and stores tag from {@link DataCollectTag} and result to {@link TargetInfo#collectedData}
+ * </p>
+ *
+ * <p>
+ * When the collecting procedure is complete, the program will launch analyze procedure with
+ * all subclass of {@link io.tomahawkd.tlstester.analyzer.Analyzer} and eventually stores
+ * analyzer tag using {@link io.tomahawkd.tlstester.analyzer.Record#column()}
+ * and result {@link TreeCode} to {@link TargetInfo#analysisResult}
+ * </p>
+ *
+ * <p>
+ * At the recording phrase, the Recorder reads the {@link TargetInfo#analysisResult} and essential
+ * data from {@link TargetInfo#collectedData} and then stores data to the database.
+ * </p>
+ */
 public class TargetInfo {
 
-	private InetSocketAddress ip;
+	/**
+	 * Contains target host and port.
+	 *
+	 * @see java.net.InetSocketAddress
+	 */
+	private InetSocketAddress host;
+
+	/**
+	 * Stores data collected from {@link DataCollector}<br>
+	 * Key: String tag from {@link DataCollectTag#tag}<br>
+	 * Value: Data
+	 */
 	private Map<String, Object> collectedData;
+
+	/**
+	 * Stores data tested from {@link io.tomahawkd.tlstester.analyzer.Analyzer}<br>
+	 * Key: String tag from {@link io.tomahawkd.tlstester.analyzer.Record#column}
+	 * Value: TreeCode result of the Analyzer
+	 */
 	private Map<String, TreeCode> analysisResult;
 
-	public TargetInfo(String ip) {
-		if (!ip.contains(":")) this.ip = new InetSocketAddress(ip, 443);
+	public TargetInfo(String host) {
+		if (!host.contains(":")) this.host = new InetSocketAddress(host, 443);
 		else {
-			String[] t = ip.split(":");
+			String[] t = host.split(":");
 			try {
-				this.ip = new InetSocketAddress(t[0], Integer.parseInt(t[1]));
+				this.host = new InetSocketAddress(t[0], Integer.parseInt(t[1]));
 			} catch (NumberFormatException e) {
-				throw new IllegalArgumentException("Invalid ip " + ip);
+				throw new IllegalArgumentException("Invalid ip " + host);
 			}
 		}
 		this.analysisResult = new HashMap<>();
@@ -28,14 +73,31 @@ public class TargetInfo {
 		return collectedData;
 	}
 
+	/**
+	 * Get host ip string.
+	 *
+	 * @return host ip.
+	 */
 	public String getIp() {
-		return ip.getAddress().getHostAddress();
+		return host.getAddress().getHostAddress();
 	}
 
+	/**
+	 * Get host ip and port.
+	 *
+	 * @return host ip:port
+	 */
 	public String getHost() {
-		return ip.getAddress().getHostAddress() + ":" + ip.getPort();
+		return host.getAddress().getHostAddress() + ":" + host.getPort();
 	}
 
+	/**
+	 * Add result from Analyzer
+	 *
+	 * @param name   String tag from {@link io.tomahawkd.tlstester.analyzer.Record#column}
+	 * @param result TreeCode result of the Analyzer
+	 * @see TargetInfo#analysisResult
+	 */
 	public void addResult(String name, TreeCode result) {
 		analysisResult.put(name, result);
 	}
@@ -44,5 +106,8 @@ public class TargetInfo {
 		return analysisResult;
 	}
 
+	/**
+	 * Default cert hash value
+	 */
 	public static final String CERT_HASH_NULL = "null";
 }
