5796d6d6a7f9fe6e1e9181f2c62ea2a81d13d7b8
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-03-22 14:12:08 +0800

    add comment

--- TLS-Tester-identifier-api/src/main/java/io/tomahawkd/tlstester/identifier/Identifier.java
+++ TLS-Tester-identifier-api/src/main/java/io/tomahawkd/tlstester/identifier/Identifier.java
@@ -2,9 +2,24 @@ package io.tomahawkd.tlstester.identifier;
 
 import com.fooock.shodan.model.host.Host;
 
+/**
+ * Identifier for identifying service/device
+ */
 public interface Identifier {
 
+	/**
+	 * Tag for identified service/device name
+	 *
+	 * @return tag
+	 */
 	String tag();
 
+	/**
+	 * identify the host
+	 *
+	 * @param host host data
+	 * @return if service/device is identified by current Identifier
+	 * @see Host
+	 */
 	boolean identify(Host host);
 }
