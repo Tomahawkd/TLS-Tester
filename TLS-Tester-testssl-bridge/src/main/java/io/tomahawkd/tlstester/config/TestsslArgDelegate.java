11993afead5de6d696738c462736ac6163aace49
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-03-23 12:45:39 +0800

    add testssl path check

--- TLS-Tester-testssl-bridge/src/main/java/io/tomahawkd/tlstester/config/TestsslArgDelegate.java
+++ TLS-Tester-testssl-bridge/src/main/java/io/tomahawkd/tlstester/config/TestsslArgDelegate.java
@@ -1,6 +1,7 @@
 package io.tomahawkd.tlstester.config;
 
 import com.beust.jcommander.Parameter;
+import io.tomahawkd.tlstester.common.FileHelper;
 
 public class TestsslArgDelegate extends AbstractArgDelegate {
 
@@ -11,4 +12,17 @@ public class TestsslArgDelegate extends AbstractArgDelegate {
 		return testsslPath;
 	}
 
+	@Override
+	public void postParsing() {
+		if (testsslPath.endsWith("/"))
+			testsslPath = testsslPath.substring(0, testsslPath.length() - 1);
+
+		if (!FileHelper.isFileExist(testsslPath + "/testssl.sh")) {
+			throw new IllegalArgumentException("testssl main program not found");
+		}
+
+		if (!FileHelper.isFileExist(testsslPath + "/openssl-iana.mapping.html")) {
+			throw new IllegalArgumentException("testssl name mapping not found");
+		}
+	}
 }
