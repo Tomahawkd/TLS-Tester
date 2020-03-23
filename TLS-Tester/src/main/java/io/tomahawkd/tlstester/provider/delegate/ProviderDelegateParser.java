438d1753a2c93abd00896b172898384e6d9f6bde
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-03-23 18:40:36 +0800

    remove unused

--- TLS-Tester/src/main/java/io/tomahawkd/tlstester/provider/delegate/ProviderDelegateParser.java
+++
@@ -1,9 +0,0 @@
-package io.tomahawkd.tlstester.provider.delegate;
-
-import io.tomahawkd.tlstester.provider.TargetProvider;
-
-public interface ProviderDelegateParser {
-	boolean identify(String type);
-
-	TargetProvider<String> parse(String v) throws Exception;
-}
