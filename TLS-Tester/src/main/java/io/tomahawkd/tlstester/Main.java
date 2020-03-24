1a1c28df057b464639c4407f84850e14641fbc9e
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-03-24 14:09:57 +0800

    introduce 3.0.0

--- TLS-Tester/src/main/java/io/tomahawkd/tlstester/Main.java
+++ TLS-Tester/src/main/java/io/tomahawkd/tlstester/Main.java
@@ -29,7 +29,7 @@ import java.util.concurrent.*;
 public class Main {
 
 	private static final Logger logger = LogManager.getLogger(Main.class);
-	private static final String version = "v2.3.5";
+	private static final String version = "v3.0.0";
 
 	static {
 		Security.addProvider(new BouncyCastleProvider());
