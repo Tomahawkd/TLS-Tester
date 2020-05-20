4c6bc28d0d4f533998d7cd21015b401b81cfbf23
Tomahawkd <20868632+Tomahawkd@users.noreply.github.com>
2020-05-20 12:53:06 +0800

    remove unnecessary dir creation

---
+++ create_keys.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# censys
+echo -n "Enter your Censys Api ID: "
+read -r API_ID
+echo ${API_ID} >> ./censys_key
+echo -n "Enter your Censys Api Secret: "
+read -r SECRET
+echo ${SECRET} >> ./censys_key
+
+# shodan
+echo -n "Enter your Shodan Api Key: "
+read API_KEY
+echo ${API_KEY} >> ./shodan_key
